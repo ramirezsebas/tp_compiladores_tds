3946cedffff5dc71ecb165393cdc74e9af6041a2
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-11-14 18:55:23 -0300

    entrada nuevo

--- bin/tds_tp.dart
+++ bin/tds_tp.dart
@@ -1,7 +1,7 @@
 import 'package:tds_tp/tds_tp.dart';
 
 void main(List<String> arguments) {
-  final String input = "olimpia campeon del mundo";
+  final String input = "olimpia campeon";
 
   Tds tds = Tds(entrada: input);
 
